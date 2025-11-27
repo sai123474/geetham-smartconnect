31aa3f1ee7755e7770e289be76ebcfb901cc13b2
sai123474 <134792253+sai123474@users.noreply.github.com>
2025-11-27 09:27:26 +0530

    Update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./authRoutes.js";
 import protectedRoutes from "./protectedRoutes.js";
+import studentRoutes from "./studentRoutes.js";
 
 const router = express.Router();
 
@@ -8,7 +9,13 @@ router.get("/health", (req, res) => {
   res.json({ status: "ok", message: "API live" });
 });
 
+// Auth
 router.use("/auth", authRoutes);
+
+// RBAC test routes
 router.use("/protected", protectedRoutes);
+
+// Students
+router.use("/students", studentRoutes);
 
 export default router;
