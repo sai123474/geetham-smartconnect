1a937915babc5baf8acbb9ada3c28503ef6fd4db
sai123474 <134792253+sai123474@users.noreply.github.com>
2025-11-27 10:02:13 +0530

    Add hall ticket routes to the main router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,6 +5,7 @@ import studentRoutes from "./studentRoutes.js";
 import attendanceRoutes from "./attendanceRoutes.js";
 import complaintRoutes from "./complaintRoutes.js";
 import feeRoutes from "./feeRoutes.js";
+import hallTicketRoutes from "./hallTicketRoutes.js";
 
 const router = express.Router();
 
@@ -21,6 +22,7 @@ router.use("/protected", protectedRoutes);
 // Students
 router.use("/students", studentRoutes);
 router.use("/attendance", attendanceRoutes);
+router.use("/hall-ticket", hallTicketRoutes);
 router.use("/complaints", complaintRoutes);
 router.use("/fees", feeRoutes);
 
