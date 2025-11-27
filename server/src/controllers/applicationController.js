5ac210dc84a3d21993ed120d803a19baf1c9edf8
sai123474 <134792253+sai123474@users.noreply.github.com>
2025-11-27 15:13:00 +0530

    Update applicationController.js

--- server/src/controllers/applicationController.js
+++ server/src/controllers/applicationController.js
@@ -4,6 +4,46 @@ import { uploadToS3 } from "../utils/s3.js";
 import { Setting } from "../models/Setting.js";
 import QRCode from "qrcode";
 import { ApplicationForm } from "../models/ApplicationForm.js";
+export const createApplicationForm = async (req, res, next) => {
+  try {
+    const {
+      academicYear,
+      name,
+      gender,
+      dob,
+      classApplyingFor,
+      parentName,
+      phone,
+      address,
+      previousSchool,
+      aadharNo
+    } = req.body;
+
+    const photo = req.file
+      ? await uploadToS3(req.file.buffer, `applicant_${Date.now()}.jpg`, req.file.mimetype, "applicants")
+      : null;
+
+    const form = await ApplicationForm.create({
+      academicYear,
+      name,
+      gender,
+      dob,
+      classApplyingFor,
+      parentName,
+      phone,
+      address,
+      previousSchool,
+      aadharNo,
+      photoUrl: photo?.Location || null,
+      status: "pending"
+    });
+
+    res.json({ status: "success", form });
+  } catch (err) {
+    next(err);
+  }
+};
+
 
 export const generateApplicationPdf = async (req, res, next) => {
   try {
