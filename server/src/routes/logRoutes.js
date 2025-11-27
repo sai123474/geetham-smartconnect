1bbe10ddf8a5ae7b8cb92cfb8ccf30e60a0b0645
sai123474 <134792253+sai123474@users.noreply.github.com>
2025-11-27 15:25:51 +0530

    Update logRoutes.js

--- server/src/routes/logRoutes.js
+++ server/src/routes/logRoutes.js
@@ -1,7 +1,7 @@
-// server/src/routes/logRoutes.js
 import express from "express";
 import { protect, restrictTo } from "../middlewares/authMiddleware.js";
 import {
+  listAuditLogs,
   exportAuditLogsCsv,
   exportAuditLogsPdf,
 } from "../controllers/auditController.js";
@@ -10,6 +10,7 @@ const router = express.Router();
 
 router.use(protect);
 router.use(restrictTo("superadmin", "dean", "principal", "admin"));
+
 router.get("/", listAuditLogs);
 router.get("/export/csv", exportAuditLogsCsv);
 router.get("/export/pdf", exportAuditLogsPdf);
