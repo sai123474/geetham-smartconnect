151d92c5e444ee249cf7d8a95e9ec1b04b62780a
sai123474 <134792253+sai123474@users.noreply.github.com>
2025-11-27 12:17:41 +0530

    Update applicationRoutes.js

--- server/src/routes/applicationRoutes.js
+++ server/src/routes/applicationRoutes.js
@@ -1,1 +1,18 @@
-router.post("/:id/generate-pdf", restrictTo("admin"), generateApplicationPdf);
+import express from "express";
+import { protect, restrictTo } from "../middlewares/authMiddleware.js";
+import {
+  createApplicationForm,
+  approveApplication,
+  generateApplicationPdf
+} from "../controllers/applicationController.js";
+import multer from "multer";
+
+const upload = multer();
+
+const router = express.Router();
+
+router.post("/", upload.single("photo"), createApplicationForm);
+router.patch("/:id/approve", protect, restrictTo("admin", "principal"), approveApplication);
+router.post("/:id/generate-pdf", protect, restrictTo("admin", "principal"), generateApplicationPdf);
+
+export default router;
