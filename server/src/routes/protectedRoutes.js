b56e0ef20591bb7f39bfd3bb594330be4344240e
sai123474 <134792253+sai123474@users.noreply.github.com>
2025-11-27 09:22:10 +0530

    Update protectedRoutes.js

--- server/src/routes/protectedRoutes.js
+++ server/src/routes/protectedRoutes.js
@@ -1,1 +1,28 @@
+import express from "express";
+import { protect, restrictTo } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+// Only logged in users
+router.get("/profile", protect, (req, res) => {
+  res.json({
+    message: `Hello ${req.user.name}, your role is ${req.user.role}`
+  });
+});
+
+// Only Admin can access
+router.get("/admin-only", protect, restrictTo("admin"), (req, res) => {
+  res.json({
+    message: "Admin route accessed successfully"
+  });
+});
+
+// Example: Only teacher and admin
+router.get("/teacher-section", protect, restrictTo("admin", "teacher"), (req, res) => {
+  res.json({
+    message: "Teacher section access granted"
+  });
+});
+
+export default router;
 
