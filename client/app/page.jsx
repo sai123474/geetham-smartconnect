28ca4b1267f34713dc3744682fbfc4da84131952
sai123474 <134792253+sai123474@users.noreply.github.com>
2025-11-28 10:05:09 +0530

    Update page.jsx

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -11,12 +11,12 @@ export default function HomePage() {
         Welcome to the Digital Academic Management System
       </p>
 
-      <Link
-        href="/dashboard"
-        className="bg-blue-600 text-white px-5 py-2 rounded-md shadow hover:bg-blue-700 transition"
-      >
-        Go to Dashboard
-      </Link>
+    <Link href="/login">
+  <button className="bg-blue-600 text-white px-6 py-3 rounded-md">
+    Login
+  </button>
+</Link>
+
     </div>
   );
 }
